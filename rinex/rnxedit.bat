22df179fdddd2efd8dd6991c198dd0c7b3e7c276
Hans van der Marel <h.vandermarel@tudelft.nl>
2025-06-29 14:27:46 +0200

    Added Windows batch file wrapper for rnxedit.pl

--- rinex/rnxedit.bat
+++ rinex/rnxedit.bat
@@ -4,8 +4,8 @@ rem For instructions:
 rem
 rem    rnxedit -h
 rem
-rem This is just a wrapper around the rnxedit.pl script for the
-rem Windows command line. If perl is not in your search path replace "perl" 
+rem This is just a wrapper around the rnxedit.pl script for the Windows 
+rem command line. If perl is not in your search path replace "perl" 
 rem the full path to the perl executable.
 
 perl %~dp0rnxedit.pl %*
